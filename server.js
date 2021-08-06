f11794e590456b6a532ea312ae457c5b35b093a9
Burke <beatty.burke@gmail.com>
2021-08-05 20:56:15 -0400

    setting up server.js and connecting heroku/mongo

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const PORT = process.env.PORT || 3000; 
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    'mongodb+srv://beatty-admin:i4hVVuD84N7a8y5@cluster0.ewnjo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
